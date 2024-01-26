5b4d4848e4af72c6561d6856ee66329f88261b3e
WELCOM <pmayya90@gmail.com>
2024-01-26 21:41:08 +0530

    Initial commit of Hello world Working copy

---
+++ src/main/java/com/example/exampleproject/ExampleProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.exampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleProjectApplication.class, args);
+        System.out.println("Hello World!!!");
+    }
+
+}
